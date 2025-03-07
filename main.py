11bac7467a8d3dec094845787be214ba443e17d5
ErgashevBoburbek <odilovabdulaziz2007@gmail.com>
2025-03-07 22:03:41 +0500

    Added Counter

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import firebase_admin
 import streamlit as st
 from colorthief import ColorThief
 from PIL import Image, ImageDraw
-import random, os, time, uuid, tomllib, json
+import random, os, time, uuid, tomllib, json, asyncio
 from firebase_admin import credentials, db
+from telegram import Bot
 
 supported_types = ["png", "jpg"]
 palettes_dir = "palettes/"
@@ -22,14 +23,22 @@ if not os.path.exists("secretinfo/info.json"):
 with open("secretinfo/info.json", "r", encoding="utf-8") as jsonforauth:
     secretdata = json.load(jsonforauth)
 database_url = secretdata["dataBase_url"]
+token = secretdata["token"]
+chatid = secretdata["chatid"]
 
 if not firebase_admin._apps:
     cred = credentials.Certificate("secretinfo/info.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': database_url
     })
-    open("auth.txt", "x")
 
+
+async def send_message():
+    bot = Bot(token=token)
+    await bot.send_message(chat_id=chatid, text=f"Username:{st.session_state.username}\nComment:{st.session_state.comment}")
+
+def submit():
+    asyncio.run(send_message())
 
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
@@ -49,6 +58,14 @@ if "download_button_disabled" not in st.session_state:
     st.session_state.download_button_disabled = True
 if "counter_ref" not in st.session_state:
     st.session_state.counter_ref = db.reference("/counter")
+if "counter" not in st.session_state:
+    st.session_state.counter = int
+if "enable_feedback" not in st.session_state:
+    st.session_state.enable_feedback = False
+if "username" not in st.session_state:
+    st.session_state.username = str
+if "comment" not in st.session_state:
+    st.session_state.comment = str
 
 
 def generate_palette_image():
@@ -79,9 +96,10 @@ def get_colors():
         st.session_state.dominant_color = process_image.get_color(quality=1)
         palette_size = max(4, st.session_state.palette_size)
         st.session_state.palette = process_image.get_palette(color_count=palette_size)
-    counter = st.session_state.counter_ref.get() or 0
-    counter += 1
-    st.session_state.counter_ref.set(counter)
+    st.session_state.counter = st.session_state.counter_ref.get() or 0
+    st.session_state.counter += 1
+    st.session_state.counter_ref.set(st.session_state.counter)
+    st.session_state.enable_feedback = True
     generate_palette_image()
 
 
@@ -94,6 +112,8 @@ def cleanup_old_files():
 
 
 cleanup_old_files()
+
+st.write(f"Number of generated images-[{st.session_state.counter_ref.get()}]")
 
 uploaded_file = st.file_uploader("Upload Image", type=supported_types)
 
@@ -131,12 +151,12 @@ if st.session_state.palette:
 palette_path = os.path.join(palettes_dir, f"{st.session_state.random_filename}.png")
 if os.path.exists(palette_path):
     with open(palette_path, "rb") as file:
-        st.download_button(
-            label="Download Image",
-            data=file,
-            file_name=f"palette_{st.session_state.random_filename}.png",
-            mime="image/png",
-        )
+        st.markdown('<div style="margin: 30px 0;">', unsafe_allow_html=True)
+        st.download_button(label="Download Image", data=file, file_name=f"palette_{st.session_state.random_filename}.png", mime="image/png")
+        st.markdown('</div>', unsafe_allow_html=True)
 
-
-st.write(f"Number of generated images: {st.session_state.counter_ref.get()}")
+if st.session_state.enable_feedback:
+    st.write("Leave Feedback")
+    st.session_state.username = st.text_input("Username", placeholder="Username")
+    st.session_state.comment = st.text_input("Comment", placeholder="Comment")
+    st.button("Submit", on_click=submit)
